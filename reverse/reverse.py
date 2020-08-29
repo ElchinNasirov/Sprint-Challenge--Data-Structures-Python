0d59054475e4631782aa9c6a51d618dfaf432884
ElchinNasirov <nasirov.elchin@gmail.com>
2020-08-28 17:56:59 -0700

    Completed reverse.py MVP

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,25 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+
+        # Set current node to head and previous node to None
+        current_node = self.head
+        prev_node = None
+
+        # Iterate through linked list, while current node does not equal none
+        while  current_node != None:
+
+            # Store next node
+            next_node = current_node.get_next()
+
+            # Set thenext of current node to previous node
+            current_node.set_next(prev_node)
+
+            # Set previous node to current node
+            prev_node = current_node
+
+            # Set current node to next node
+            current_node = next_node
+
+        # While current node does equal none set head to previous node
+        self.head = prev_node
